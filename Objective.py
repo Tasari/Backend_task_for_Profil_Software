dfe71635c91e1c08b8db026594e85eef96e3ddf0
Tasari <46652399+Tasari@users.noreply.github.com>
2019-11-02 23:04:50 +0100

    Finished all methods, starting creating console commands

--- Objective.py
+++ Objective.py
@@ -2,7 +2,8 @@ import sqlite3
 from sqlite3 import Error
 import requests
 import ast
-
+import re
+import sys
 class Movies_Info:
     def __init__(self, database):
         conn = None
@@ -14,6 +15,7 @@ class Movies_Info:
         self.cur = conn.cursor()        
         self.cur.execute('PRAGMA table_info(MOVIES)')
         self.table_columns_info = self.cur.fetchall()
+        self.last_fetch = 0
         print(self.table_columns_info)
 
     def print_database(self):
@@ -21,8 +23,12 @@ class Movies_Info:
         Print all rows in the tasks table
         '''
         self.cur.execute("SELECT * FROM MOVIES")
-        rows = self.cur.fetchall()
-        for row in rows:
+        self.last_fetch = self.cur.fetchall()
+        for row in self.last_fetch:
+            print(row)
+
+    def print_last_fetch(self):
+        for row in self.last_fetch:
             print(row)
 
     def update_single_movie(self, Title):
@@ -61,11 +67,26 @@ class Movies_Info:
         rows = self.cur.fetchall()
         for row in rows:
             self.update_single_movie(row[1])
-    def sort_database(self, columns={'ID':'ASC'}):
+            
+    def sort_database(self, columns={'ID':'ASC'}, titles=0, out=0):
         '''
         Sorting database by multiple columns, main column is given as argument
         '''
-        command = "SELECT * FROM MOVIES ORDER BY "
+        string=''
+        for table_col in columns.keys():
+            if table_col == 'CAST':
+                x = '"{}"'.format(table_col)
+                string += '{}, '.format(x)
+            else:
+                string += '{}, '.format(table_col)
+        string = string[:-2] + ' '
+        command = "SELECT TITLE, {}FROM MOVIES ".format(string)
+        if titles:
+            command += 'WHERE '
+            for title in titles:
+                command += 'TITLE = "{}" OR '.format(title)
+            command = command[:-3]
+        command += 'ORDER BY '
         for col in columns.keys():
             if col == 'BOX_OFFICE':
                 added_condition = "CAST(REPLACE(SUBSTR({},2), ',', '') AS FLOAT) {}, ".format(col, columns[col])
@@ -73,18 +94,23 @@ class Movies_Info:
                 added_condition = "CAST({} AS UNSIGNED) {}, ".format(col, columns[col])
             elif col == 'IMDb_votes':
                 added_condition = 'CAST(REPLACE({}, ",", "") AS UNSIGNED) {}, '.format(col, columns[col])
+            elif col == 'CAST':
+                x = '"{}"'.format(col)
+                added_condition = '{} {}, '.format(x, columns[col])
             else:
                 added_condition = "{} {}, ".format(col, columns[col])
             command += added_condition
+
         command = command[:-2] + ';'
-        print(command)
         self.cur.execute(command)
-        rows = self.cur.fetchall()
-        for row in rows:
-            print(row)
-    def filtered_data(self, column, data):
+        self.last_fetch = self.cur.fetchall()
+        if out: 
+            for row in self.last_fetch:
+                print(row)
+
+    def filtered_data(self, column, data, out=0):
         if data == 'Ratio':
-            movies_with_good_ratio = []
+            movies_with_good_ratio = {}
             x = '"{}"'.format(column)
             self.cur.execute("SELECT TITLE, AWARDS FROM MOVIES")
             all_movies_awards = self.cur.fetchall()
@@ -92,43 +118,115 @@ class Movies_Info:
             for movie_award in all_movies_awards:
                 all_numbers = []
                 for word in movie_award[1].split():
-                    print(type(word))
                     if word.isdigit():
                         all_numbers.append(word)
-                    print(all_numbers)
-     #           if len(all_numbers)>1:  
-                print(movie_award[0])  
-                ratio = int(all_numbers[len(all_numbers)-2])/int(all_numbers[len(all_numbers)-1])
-                print(ratio)
-
+                if len(all_numbers)>1:  
+                    ratio = int(all_numbers[len(all_numbers)-2])/int(all_numbers[len(all_numbers)-1])
                 if ratio > 0.8:
-                    movies_with_good_ratio.append(all_movies_awards[i][0])
+                    movies_with_good_ratio[all_movies_awards[i][0]]=ratio
                 i+=1
-            print(movies_with_good_ratio)
+            command = "SELECT TITLE FROM MOVIES WHERE "
+            for movie_with_good_ratio in movies_with_good_ratio.keys():
+                command = command + "TITLE = '{}' OR ".format(movie_with_good_ratio)
+            command = command[:-4]+";"
+            print(command)
+            self.cur.execute(command)
+            self.last_fetch = self.cur.fetchall()
+            if out:
+                for row in self.last_fetch:
+                    print(row[0] + " Ratio: " + str(movies_with_good_ratio[row[0]]))
         elif data == 'Income':
             self.cur.execute("SELECT TITLE, BOX_OFFICE FROM MOVIES")
             rows = self.cur.fetchall()
-            high_income = []
+            high_income = {}
             for row in rows:
                 income = row[1]
                 x = income.replace("$", "")
                 z = x.replace(",", "")
                 if income != 'N/A':
                     if int(z) > 100000000:
-                        high_income.append(row[0])
-            print(high_income)
+                        high_income[row[0]]=income
+            command = "SELECT TITLE, BOX_OFFICE FROM MOVIES WHERE "
+            for high_income_movie in high_income.keys():
+                command = command + "TITLE = '{}' OR ".format(high_income_movie)
+            command = command[:-4]+";"
+            print(command)
+            self.cur.execute(command)
+            self.last_fetch = self.cur.fetchall()
+        elif data == 'NomOsc':
+            self.cur.execute("SELECT TITLE, AWARDS FROM MOVIES WHERE AWARDS LIKE 'Nomi%Oscar%';")
+            self.last_fetch = self.cur.fetchall()
         else:
             x = '"{}"'.format(column)
             print(x)
-            self.cur.execute("SELECT * FROM MOVIES WHERE instr({}, ?)".format(x), (data,))
-            rows = self.cur.fetchall()
-            for row in rows:
+            self.cur.execute("SELECT TITLE, {} FROM MOVIES WHERE instr({}, ?)".format(column, x), (data,))
+            self.last_fetch = self.cur.fetchall()
+        if out:
+            for row in self.last_fetch:
                 print(row)
+    def add_movie_to_database(self, title):
+        self.cur.execute("INSERT INTO MOVIES (TITLE) VALUES(?);",(title,))
+        self.update_single_movie(title)
 
+    def find_with_regex(self, regex, compared_movies=0):
+        command = "SELECT TITLE, AWARDS FROM MOVIES WHERE "
+        if compared_movies:
+            for title in compared_movies:
+                command += 'TITLE = "{}" OR '.format(title)
+                command = command[:-4]
+            self.cur.execute(command)
+        else:
+            self.cur.execute("SELECT TITLE, AWARDS FROM MOVIES")
+        self.last_fetch = self.cur.fetchall()
+        expression = re.compile(regex)
+        print(expression)
+        all_awards = {}
+        for row in self.last_fetch:
+            found = expression.findall(row[1])
+            if len(found):
+                words = found[0].split()
+                for word in words:
+                    try:
+                        all_awards[row[0]] = int(word)
+                    except ValueError:
+                        pass
+        max_val = max(all_awards.values())
+        best = {}
+        for key in all_awards:
+            if all_awards[key] == max_val:
+                best[key] = all_awards[key]
+        return best
+            
+    def compare_movies(self, attribute, compared_ones=0):
+        if attribute == 'MOST_NOMINATIONS':
+            all_found = self.find_with_regex('\d+ nomina', compared_ones)
+            print(all_found)
+        elif attribute == 'ALL_AWARDS':
+            all_found = self.find_with_regex('\d+ win', compared_ones)
+            print(all_found)
+        elif attribute == 'OSCARS':
+            all_found = self.find_with_regex('on \d+ Osc', compared_ones)
+            print(all_found)
+        elif compared_ones:
+            self.sort_database({attribute:'DESC'}, compared_ones)
+            print(self.last_fetch[0])
+        else:
+            self.sort_database({attribute:'DESC'})
+            print(self.last_fetch[0])
 
+    def high_scores(self):
+        self.compare_movies('RUNTIME')
+        self.compare_movies('BOX_OFFICE')
+        self.compare_movies('MOST_NOMINATIONS')
+        self.compare_movies('ALL_AWARDS')
+        self.compare_movies('OSCARS')
+        self.compare_movies('IMDb_Rating')
 Movies = Movies_Info("movies.sqlite")
 Movies.update_database()
-#Movies.sort_database({'IMDb_Rating':'DESC', 'IMDb_votes':'DESC' })
-Movies.filtered_data('AWARDS', 'Income')
-
-
+if sys.argv[1] == '--sort_by':
+    given_columns = {}
+    for argument in sys.argv[2:]:
+        given_columns[argument.upper()] = 'DESC'
+    Movies.sort_database(given_columns)
+    for column in Movies.last_fetch:
+        print(column[0] +"|"+str(column[1]))
